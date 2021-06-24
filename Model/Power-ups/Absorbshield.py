17c0078ae4a4e60cbf3f9c68001192b3f9f16a74
zjovy <65920699+zjovy@users.noreply.github.com>
2021-06-24 16:41:46 -0400

    Update Absorbshield.py

--- Model/Power-ups/Absorbshield.py
+++ Model/Power-ups/Absorbshield.py
@@ -4,7 +4,8 @@ import Powerup
 # replaces shield with a shield that can absorb incoming projectiles
 class Absorb_shield (Powerup):
   
-  def __init__(self, type):
+  def __init__(self):
+    Powerup.__init__(self, type)
     self.type = "shield"
   
   # absorbing projectiles can heal the player
