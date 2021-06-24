6ded1258529143f1bae033ac15a9a7008a521e6e
zjovy <65920699+zjovy@users.noreply.github.com>
2021-06-24 16:05:46 -0400

    Update Bounceshield.py

--- Model/Power-ups/Bounceshield.py
+++ Model/Power-ups/Bounceshield.py
@@ -1,6 +1,6 @@
 import game
 
-# instead of blocking incoming projectiles
+# instead of blocking incoming projectiles with the shield
 # this power up allows the shield to reflect projectiles back into enemies
 def bounce_shield():
   
