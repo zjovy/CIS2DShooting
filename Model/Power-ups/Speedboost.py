8b2f312db89dc366de56a6863da5d3d7e2f8736d
zjovy <65920699+zjovy@users.noreply.github.com>
2021-06-24 16:32:04 -0400

    Update Speedboost.py

--- Model/Power-ups/Speedboost.py
+++ Model/Power-ups/Speedboost.py
@@ -1,12 +1,18 @@
 import game
+import Powerup
 
-# increase in speed
-def speed_boost ():
-  if(move_right):
-        x += 4
-    if(move_left):
-        x -= 4
-    if(move_up):
-        y -= 4
-    if(move_down):
-        y += 4
+# increases player's walk speed
+class SpeedBoost(Powerup):
+  
+  def __init__ (self, type):
+    self.type = "movement"
+  
+  def speed_boost ():
+    if(move_right):
+          x += 4
+      if(move_left):
+          x -= 4
+      if(move_up):
+          y -= 4
+      if(move_down):
+          y += 4
