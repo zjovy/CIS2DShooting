a0467d083fdfd7d461522dd9280d4ce8d1e8f783
zjovy <65920699+zjovy@users.noreply.github.com>
2021-06-24 15:51:59 -0400

    Create Speedboost.py

---
+++ Model/Power-up/Speedboost.py
@@ -0,0 +1,14 @@
+import pygame
+import game
+import random
+
+# increase in speed
+def speed_boost ():
+  if(move_right):
+        x += 4
+    if(move_left):
+        x -= 4
+    if(move_up):
+        y -= 4
+    if(move_down):
+        y += 4
