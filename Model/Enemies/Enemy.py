e4bc1d8a013eff43149f777c564880ee8edbbf52
zjovy <65920699+zjovy@users.noreply.github.com>
2021-06-24 16:38:14 -0400

    Create Enemy.py

---
+++ Model/Enemies/Enemy.py
@@ -0,0 +1,17 @@
+class Enemy:
+  
+  def __init__ (self, damage, score):
+    self.health = 100
+    self.damage = damage
+    self.score = score
+   
+  def attack(self, opponent):
+    damage_dealt = self.damage
+    opponent.take_damage(damage_dealt)
+    
+  def take_damage(self, damage_dealt):
+    self.health -= damage_dealt
+    
+  def alive(self):
+    return self.health > 0
+  
