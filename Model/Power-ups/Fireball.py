0d5df2687d9ce34eff34ecf2074df115fd527208
zjovy <65920699+zjovy@users.noreply.github.com>
2021-06-24 16:28:43 -0400

    Update Fireball.py

--- Model/Power-ups/Fireball.py
+++ Model/Power-ups/Fireball.py
@@ -1,7 +1,12 @@
 import game
+import Powerup
 
 # change the attack into shooting a fireball
-# deals continuous fire damage
-def fireball():
+class Fireball(Powerup):
   
-  pass
+  def __init__ (self, type):
+    self.type = "attack"
+    
+  # deals continuous burning damage to enemies
+  def burning (enemy):
+    pass
