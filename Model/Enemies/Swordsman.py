0104b71b18673170f233d881f1551b7b2fcc75b4
zjovy <65920699+zjovy@users.noreply.github.com>
2021-06-24 16:48:57 -0400

    Update Swordsman.py

--- Model/Enemies/Swordsman.py
+++ Model/Enemies/Swordsman.py
@@ -3,8 +3,8 @@ import Enemy
 # melee enemy, short range attacks
 class Swordsman (Enemy):
   
-  def  __init__ (self, range):
+  def  __init__ (self):
     Enemy.__init__(self, damage, score)
-    self.range = range
     self.damage = 10
+    self.range = 5
     self.score = 30
