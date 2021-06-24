0616b3f1d46a63ccb79b1caaf74fccdc488bac26
zjovy <65920699+zjovy@users.noreply.github.com>
2021-06-24 16:43:05 -0400

    Update Ninjastar.py

--- Model/Power-ups/Ninjastar.py
+++ Model/Power-ups/Ninjastar.py
@@ -1,10 +1,10 @@
-import game
 import Powerup
 
 # replaces attack with shooting a ninja star
 class Ninjastar(Powerup):
   
-  def __init__ (self, type):
+  def __init__ (self):
+    Powerup.__init__(self, type)
     self.type = "attack"
     
   # ninja star has fast projectile speed, which means that it travels faster  
